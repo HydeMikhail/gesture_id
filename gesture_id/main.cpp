c886ae9737db2ad4458c599c7629c61a2408e1b7
Mikhail Hyde <mikhail.hyde@formlabs.com>
2023-06-18 21:46:21 -0400

    MVP Camera object

--- gesture_id/main.cpp
+++ gesture_id/main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
-#include <csignal>
+#include <condition_variable>
+#include <mutex>
 
 #include <libcamera/libcamera.h>
 #include <libcamera/camera_manager.h>
 #include <libcamera/framebuffer.h>
+
+namespace GestureId::Hardware::Camera {
 
 enum class CameraState {
     AVAILABLE = 0,
@@ -16,111 +19,154 @@ class Camera {
 public:
     Camera() {
         ConfigureManager();
+        AcquireCamera();
         ConfigureCamera();
-        ConfigureBuffer();
-        camera->start();
     }
 
     ~Camera() {
-        StopAll();
-    }
-
-    void PrintAvailableCameras() {
-        std::cout << "" << std::endl;
-        std::cout << "Cameras Found: " << manager->cameras().size() << std::endl;
-        std::cout << "" << std::endl;
-        std::cout << "Names of Available Cameras:" << std::endl;
-        for (const std::shared_ptr<libcamera::Camera> cam : manager->cameras()) {
-            std::cout << cam->id() << std::endl;
-        }
-        std::cout << "" << std::endl;
-    }
-
-    void StopAll() {
-        // Stop LibCamera Objects
-        camera->stop();
-        camera->release();
-        manager->stop();
-        // Release Unique PTRs
-        camera.reset();
-        manager.release();
-        allocator.release();
-        return;
+        StopCamera();
+        Stop();
     }
     
-    void Run() {
+    void StartCamera() {
+        if (cameraState == CameraState::CONFIGURED) {
+            camera->start();
+        }
+    }
+    
+    void StopCamera() {
+        if (cameraState == CameraState::CONFIGURED) { camera->stop(); }
+        if (camera) {
+            camera->requestCompleted.disconnect();
+            requests.clear();
+        }
+        if (allocator) {
+            for (libcamera::StreamConfiguration& cfg : *config) {
+                allocator->free(cfg.stream());
+            }
+            allocator.release();
+        }
+        if (cameraState != CameraState::AVAILABLE) {
+            camera->release();
+            camera.reset();
+            cameraState = CameraState::AVAILABLE;
+        }
+    }
 
+    // TODO: Revisit data movement in this method
+    void Run() {
+        StartCamera();
+        {
+            std::unique_lock lock{frameWaitMutex};
+            QueueFrameRequest();
+            
+            if (frameWaitCV.wait_for(lock, std::chrono::milliseconds{1000}) == std::cv_status::timeout) {
+                return;
+            }
+        }
+        processFrameFunctionPointer();
     }
 
 private:
-    static inline void RequestCallback(libcamera::Request* request) {
-        std::cout << "Callback" << std::endl;
-    }
-
-    int ConfigureManager() {
-        // Configure Manager - Must be manually defined before proceeding
+    bool ConfigureManager() {
         manager = std::make_unique<libcamera::CameraManager>();
         manager->start();
-        if (manager->cameras().empty()) { return -1; }
-        return 0;
+        if (manager->cameras().empty()) { return false; }
+        return true;
     }
 
-    int ConfigureCamera() {
-        // Identify camera from manager and define camera object
-        std::string id = manager->cameras()[0]->id();
-        camera = manager->get(id);
-        camera->acquire();
-
-        // Define the camera configuration
-        config = camera->generateConfiguration({libcamera::StreamRole::VideoRecording});
-        config->at(0).size.width = 480;
-        config->at(0).size.height = 480;
-        config->at(0).pixelFormat = libcamera::formats::BGR888;
-        config->validate();
-        camera->configure(config.get());
-        return 0;
+    bool AcquireCamera() {
+        if (cameraState == CameraState::AVAILABLE) {
+        if (manager->cameras().empty()) { return false; }
+        auto cameraID = manager->cameras()[0]->id();
+        camera = manager->get(cameraID);
+        if (camera->acquire()) { return false; }
+            else { cameraState = CameraState::ACQUIRED; }
+        }
+        return true;
     }
 
-    int ConfigureBuffer() {
-        // Configure FrameBufferAllocator - Must be manually defined before proceeding
+    bool ConfigureCamera() {
+        if (cameraState == CameraState::ACQUIRED) {
+            config = camera->generateConfiguration({libcamera::StreamRole::VideoRecording});
+            config->at(0).size.width = 480;
+            config->at(0).size.height = 480;
+            config->at(0).pixelFormat = libcamera::formats::BGR888;
+            config->validate();
+            camera->configure(config.get());
+            cameraState = CameraState::CONFIGURED;
+        }
+        return SetupFrameBuffer();
+    }
+
+    bool SetupFrameBuffer() {
+        if (allocator) { return true; }
         allocator = std::make_unique<libcamera::FrameBufferAllocator>(camera);
-        std::vector<std::unique_ptr<libcamera::Request>> requests;
-        // Allocate buffers for each stream in camera config
         for (const libcamera::StreamConfiguration& cfg: *config) {
-            if (allocator->allocate(cfg.stream()) < 0) { return -1; }
+            if (allocator->allocate(cfg.stream()) < 0) { return false; }
             size_t allocated = allocator->buffers(cfg.stream()).size();
         }
         libcamera::Stream* stream = config->at(0).stream();
         const std::vector<std::unique_ptr<libcamera::FrameBuffer>>& buffers = allocator->buffers(stream);
-        if (buffers.empty()) { return -1; }
-        // Allocate frame request for each stream
+        if (buffers.empty()) { return false; }
         for (uint16_t i = 0; i < buffers.size(); i++) {
             std::unique_ptr<libcamera::Request> request = camera->createRequest();
-            if (!request) { return -1; }
+            if (!request) { return false; }
             const std::unique_ptr<libcamera::FrameBuffer>& buffer = buffers[i];
-            if (request->addBuffer(stream, buffer.get()) < 0) { return -1; }
+            if (request->addBuffer(stream, buffer.get()) < 0) { return false; }
             requests.push_back(std::move(request));
         }
-        camera->requestCompleted.connect(this, &RequestCallback);
-        return 0;
+        camera->requestCompleted.connect(this, &Camera::RequestCallback);
+        return true;
+    }
+    
+    void RequestCallback(libcamera::Request* request) {
+        if (request->status() == libcamera::Request::RequestCancelled) {
+            frameWaitCV.notify_all();
+            return;
+        }
+        processFrameFunctionPointer = std::bind(&Camera::ProcessFrame, this, request);
+        frameWaitCV.notify_all(); 
+    }
+    
+    void QueueFrameRequest() {
+        for (std::unique_ptr<libcamera::Request>& request : requests) {
+            camera->queueRequest(request.get());
+        }
+    }
+
+    // TODO: Implement actual logic in this function
+    void ProcessFrame(libcamera::Request* request) {
+        const libcamera::Request::BufferMap& buffers = request->buffers();
+        for (auto [stream, buffer] : buffers) {
+            const libcamera::FrameMetadata& metadata = buffer->metadata();
+        }
+        camera->stop();
+    }
+
+    void Stop() {
+        config.reset();
+        manager->stop();
     }
 
 private:
-    // Define required libcamera objects globally (for example)
-    // CameraManager - https://libcamera.org/api-html/classlibcamera_1_1CameraManager.html
     std::unique_ptr<libcamera::CameraManager> manager;
-    // CameraConfiguration - https://libcamera.org/api-html/classlibcamera_1_1CameraConfiguration.html
     std::unique_ptr<libcamera::CameraConfiguration> config;
-    // Camera - https://libcamera.org/api-html/classlibcamera_1_1Camera.html
     std::shared_ptr<libcamera::Camera> camera;
-    // FrameBufferAllocator - https://libcamera.org/api-html/classlibcamera_1_1FrameBufferAllocator.html
     std::unique_ptr<libcamera::FrameBufferAllocator> allocator;
-};
+    std::vector<std::unique_ptr<libcamera::Request>> requests;
 
+    std::function<void()> processFrameFunctionPointer;
+
+    std::condition_variable frameWaitCV;
+    std::mutex frameWaitMutex;
+
+    CameraState cameraState = CameraState::AVAILABLE;
+};
+} // namespace GestureId::Hardware::Camera
 
 int main(int argc, const char** argv) {
-    Camera camera;
-    camera.PrintAvailableCameras();
+    GestureId::Hardware::Camera::Camera camera;
     camera.Run();
     return 0;
 }
